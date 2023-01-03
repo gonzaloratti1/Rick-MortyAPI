1ad0bc521377a55a417cfa332b4a7ce11a6598e8
gonzaloratti1 <gonzaloratti1@gmail.com>
2023-01-03 15:10:39 -0300

    first comm

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import Character from './components/Character';
 
 function App() {
 
-    return <div className='bg-dark-text-white'>
-      <h1 className='text-center display-1 py-4'>Rick and Morty</h1>
+  return <div className='bg-dark-text-white'>
+    <h1 className='text-center display-1 py-4'>Rick and Morty</h1>
 
-      <CharacterList/>
-      
+    <CharacterList />
 
-    </div>
-  
+
+  </div>
+
 }
 
 export default App
